83892398e4cecd7acf188a235b68c331521fe7f0
AfnanAhmadTariq <116060169+AfnanAhmadTariq@users.noreply.github.com>
2023-10-14 00:30:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function updateDisplay() {
     var userInput = document.getElementById("userInput").value;
     var displayArea = document.getElementById("displayArea");
     displayArea.textContent = userInput;
-    displayArea.textContent = Caclulator.calSimple(userInput).toString();
+    // displayArea.textContent = calSimple(userInput);
 }
+   
